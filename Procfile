1e222766fde78146e1e34baa37c23353477998f4
rajesh67 <rajeshmeena.iitkgp@gmail.com>
2018-03-24 04:12:58 +0530

    First Heroku Deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn statsbot_project.production_wsgi --log-file -
--- statsbot_project/settings.py
+++ statsbot_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'f57s+v#&#75*8)bxmiwr^#sk#=k$e%ef(-@65slmljx=p3ic+y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '*', 'statsbot.org']
 
 
 # Application definition
@@ -86,6 +86,7 @@ WSGI_APPLICATION = 'statsbot_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#=============lOCALHOST DATABSE CONFIGURATIONS===============
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -96,8 +97,9 @@ WSGI_APPLICATION = 'statsbot_project.wsgi.application'
 #         'PORT' : '',
 #     }
 # }
+#=============lOCALHOST DATABSE =============================
 
-
+#=============Heroku configuration DATABASE==================
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -105,6 +107,9 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+#==============================================================
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
