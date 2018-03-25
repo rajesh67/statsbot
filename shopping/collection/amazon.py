881ec75faf64ae034975f65ae9f4ba9c99844f68
rajesh67 <rajeshmeena.iitkgp@gmail.com>
2018-03-25 10:27:41 +0530

    more improved footer

--- shopping/collection/amazon.py
+++ shopping/collection/amazon.py
@@ -1,5 +1,23 @@
-from urllib.urlparse import urlparse, parse_qs
+from urllib.parse import urlparse, parse_qs
+
+from django.conf import settings
+from shopping.models import (
+	Store,
+	SearchProduct,
+	SearchProductImage,
+)
+import bottlenose
+import json
+
 
 class AmazonSearchAPIHandler():
-	def register(self):
+	def __init__(self, *args, **kwargs):
+		self.store=Store.objects.get(short_name="amazon")
+		self.amazon_handle=bottlenose.Amazon(self.store.affiliate_id, self.store.affiliate_token, "statsbot.org-21", Region= 'IN')
+
+	def get_search_results(self, keywords):
+		resp=self.amazon_handle.ItemSearch(Keywords=keywords, SearchIndex='All')
+		return resp
+
+	def parse_results_xml(self, resp):
 		pass
