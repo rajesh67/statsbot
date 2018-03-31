68af680f29710d2ac915def1f668178bbf9a8d4a
rajesh67 <rajeshmeena.iitkgp@gmail.com>
2018-03-31 15:22:31 +0530

    akfa;fj;aklfa;

--- statsbot_project/urls.py
+++ statsbot_project/urls.py
@@ -39,6 +39,6 @@ urlpatterns = [
 urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if not settings.DEBUG:
+if settings.DEBUG:
 	urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 	urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
