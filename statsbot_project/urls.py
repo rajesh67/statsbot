d00991dcee9a0647ad6560cf9e0f5dea5a98f4e5
rajesh67 <rajeshmeena.iitkgp@gmail.com>
2018-04-01 21:41:33 +0530

    fixed 2

--- statsbot_project/urls.py
+++ statsbot_project/urls.py
@@ -40,6 +40,6 @@ urlpatterns = [
     url(r'^hotels/', include('hotels.urls')),
 ]
 
-if not settings.DEBUG:
+if settings.DEBUG:
 	urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 	urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
